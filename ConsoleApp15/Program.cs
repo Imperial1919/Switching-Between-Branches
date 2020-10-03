2752e3da951c8ddc795389987bf0a173cc0d88f8
Bryce <ImperialEagle1998@gmail.com>
2020-10-02 19:06:54 -0500

    A little fun with Branch Switching.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 3;
+
+            string day;
+
+            switch (num)
+            {
+                case 1:
+                    day = "Monday";
+                    break;
+
+                case 2:
+                    day = "Tuesday";
+                    break;
+
+                case 3:
+                    day = "Wednesday";
+                    break;
+
+                case 4:
+                    day = "Thursday";
+                    break;
+
+                case 5:
+                    day = "Friday";
+                    break;
+
+                case 6:
+                    day = "Saturday";
+                    break;
+
+                case 7:
+                    day = "Friday";
+                    break;
+
+                default:
+                    day = "Unknown, ERROR!";
+                    break;
+            }
+
+
+            Console.WriteLine("Hello Sir! It is June the 26th, 1945 today! The Current day of the week is " + day + " and it is 6:38 PM.");
+            Console.ReadLine();
+
+        }
+    }
+}
